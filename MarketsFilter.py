0cfcd8552ce248f77e17e173355a39bcc1940186
dzordanq <random05510@gmail.com>
2020-09-29 14:06:28 +0200

    filters update

--- MarketsFilter.py
+++ MarketsFilter.py
@@ -5,7 +5,10 @@ class MarketFilter:
     def FilterMarkets(markets):
         markets = [bet_offer for bet_offer in markets
                         if bet_offer['criterion']['englishLabel'] in MARKETS.keys()
-                        or ('by' in bet_offer['criterion']['englishLabel'] and 'Odd/Even' not in bet_offer['criterion']['englishLabel'])]
+                        or ('Total Corners by' in bet_offer['criterion']['englishLabel'] and 'Odd/Even' not in bet_offer['criterion']['englishLabel'])
+                        or ('Total Cards by' in bet_offer['criterion']['englishLabel'] and 'Odd/Even' not in bet_offer['criterion']['englishLabel'])
+                        or ('Total Goals by' in bet_offer['criterion']['englishLabel'] and 'Odd/Even' not in bet_offer['criterion']['englishLabel'])
+            ]
         return markets
         
 
